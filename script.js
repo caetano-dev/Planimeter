8bb647e65a9d71614a89fd0d32f877008c280b1d
Drull <pcaetanop@protonmail.com>
2023-01-13 10:05:14 -0300

    draw red lines to fill

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const calculateAreaButton = document.querySelector('.area');
+const newAreaDiv = document.querySelector('.newArea');
 const coordinates = document.querySelector('#coordinates');
 const resultTextArea = document.querySelector('#result');
 const clearButton = document.querySelector('.clear');
@@ -17,8 +18,38 @@ function addClick(x, y, drag) {
   coord.push({ X: x, Y: y, DRAG: drag });
 }
 
-function redraw() {
-  con.clearRect(0, 0, con.canvas.width, con.canvas.height);
+const showCoordinates = (x, y, canvasX, canvasY) => {
+  coordinates.innerHTML = "(" + canvasX + "px, " + canvasY + "px)";
+  coordinates.style.top = y-50 + "px";
+  coordinates.style.left = x+50 + "px";
+}
+
+let startPoint 
+let lastPoint 
+
+function redraw(e) {
+  mx = e.offsetX;
+  my = e.offsetY;
+
+  // draw a new rect from the start position 
+  // to the current mouse position
+  con.beginPath();
+  con.lineWidth = 8;
+  con.lineJoin = con.lineCap = 'round';
+  con.setLineDash([0, 0]);
+  con.globalAlpha = 1.0;
+
+
+  con.moveTo(startPoint.x, startPoint.y);
+  con.lineTo(mx, my);
+
+  con.closePath();
+  con.strokeStyle = 'red';
+  con.fill();
+  con.stroke();
+  con.fillStyle = 'rgba(25,50,75,0.5)';
+  con.fill();
+
   for(let i = 0; i < coord.length; i++) {
     con.beginPath();
     if (coord[i].DRAG) {
@@ -28,30 +59,27 @@ function redraw() {
     }
     con.lineTo(coord[i].X, coord[i].Y);
     con.closePath();
-    con.lineWidth = 3;
+    con.lineWidth = 8;
     con.strokeStyle = 'rgb(0%, 0%, 0%)';
+    con.fill();
     con.stroke();
   }
 }
 
-function calculateArea(coord) {
-  let area = 0;
-  for (let i = 0; i < coord.length - 1; i++) {
-    let x = coord[i].X;
-    let y = coord[i].Y;
-    let nextX = coord[i + 1].X;
-    let nextY = coord[i + 1].Y;
-    area += x * nextY - y * nextX;
-  }
-  return Math.abs(area / 2);
-}
-
 can.addEventListener('mousedown', function(e) {
-  mouseX = e.pageX - this.offsetLeft;
-  mouseY = e.pageY - this.offsetTop;
+  con.clearRect(0, 0, con.canvas.width, con.canvas.height);
+  startX = e.clientX - this.offsetLeft;
+  startY = e.clientY - this.offsetTop;
+  addClick(startX, startY, false); 
+  con.beginPath();
+  con.moveTo(e.clientX, e.clientY);
   paint = true;                     
-  addClick(mouseX, mouseY, false); 
-  redraw();                       
+  lastPoint = {
+    x: e.offsetX,
+    y: e.offsetY
+  };
+  startPoint = lastPoint;
+  redraw(e);                       
 });
 
 can.addEventListener('mousemove', function(e) {
@@ -62,36 +90,39 @@ can.addEventListener('mousemove', function(e) {
   let canvasY = Math.round(e.clientY - cRect.top);
   canvasX = canvasX - 36
   canvasY = can.height - canvasY - 28
-  let x = event.clientX;
-  let y = event.clientY;
-  coordinates.innerHTML = "(" + canvasX + "px, " + canvasY + "px)";
-  coordinates.style.top = y-30 + "px";
-  coordinates.style.left = x+40 + "px";
+  let x = e.clientX;
+  let y = e.clientY;
+  showCoordinates(x, y, canvasX, canvasY);
 
   if (paint) {              
     addClick(mouseX, mouseY, true);
-    redraw();
+    redraw(e);
   }
 });
 
 can.addEventListener('mouseup' || 'mouseleave', function(e) {
   paint = false;                            
+  con.lineTo(startPoint.x, startPoint.y);
+  con.closePath();
+  con.stroke()
+  con.fill();
+
+  let imageData = con.getImageData(0, 0, can.width, can.height);
+  let data = imageData.data;
+  let totalPixels = data.length / 4;
+  let pixelsInside = 0;
+  let r = 1000;
+  let d = 1;
+
+  for (let i = 0; i < totalPixels; i++) {
+    let isInside = data[i * 4 + 3] > 0; // check if the pixel is inside the closed shape
+    if (isInside) {
+      pixelsInside++;
+    }
+  }
+  let theta = (pixelsInside / totalPixels) * 2 * Math.PI;
+  let area = (r * theta * Math.PI) / 360 * d;
+  console.log("Area of the closed shape:", area);
+
 });
-
-calculateAreaButton.addEventListener("click", function(){
-  const areaOfBackgroundSquare = 2500;
-  resultText = `Valor da área: ${calculateArea(coord)/areaOfBackgroundSquare} cm²`;
-  resultTextArea.innerText = resultText;
-})
-
-function drawSquare(x, y, sideLength, drag, con) {
-  coord.push({ X: x, Y: y, DRAG: drag });
-  coord.push({ X: (x+sideLength), Y: y, DRAG: drag });
-  coord.push({ X: (x+sideLength), Y: (y+sideLength), DRAG: drag });
-  coord.push({ X: x, Y: (y+sideLength), DRAG: drag });
-  coord.push({ X: x, Y: y, DRAG: drag });
-  con.fillRect(x, y, sideLength, sideLength);
-}
-
-drawSquare(634, 322, 100, false, con);
 
